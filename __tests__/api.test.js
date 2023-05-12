f582d265ea7293a70c87cfc31afc207789295d87
Charlie Storer <charliestorer1997@gmail.com>
2023-05-12 09:29:30 +0100

    Ticket 5 ammendments

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -4,6 +4,7 @@ const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data");
 const db = require("../db/connection");
 const endpoint = require("../endpoints.json");
+const sorted = require("jest-sorted");
 
 afterAll(() => {
   return db.end();
@@ -65,39 +66,38 @@ describe("Returns with the correct JSON endpoint", () => {
       .get(`/api/reviews/${review_id}`)
       .expect(200)
       .then((response) => {
-        response.body.review.forEach((review) => {
-          expect(typeof review.title).toBe("string");
-          expect(typeof review.created_at).toBe("string");
-          expect(typeof review.votes).toBe("number");
-          const input = Object.keys(
-            endpoint["GET /api/reviews/:review_id"].exampleResponse
-          );
-          const expectedOutput = Object.keys(response.body);
-          expect(input).toMatchObject(expectedOutput);
-        });
+        const review = response.body.review[0];
+        expect(typeof review.title).toBe("string");
+        expect(typeof review.created_at).toBe("string");
+        expect(typeof review.votes).toBe("number");
+        expect(typeof review.review_id).toBe("number");
+        expect(typeof review.designer).toBe("string");
+        expect(typeof review.review_img_url).toBe("string");
+        expect(typeof review.category).toBe("string");
+        expect(typeof review.review_body).toBe("string");
+        expect(typeof review.owner).toBe("string");
       });
   });
-  test("/api/reviews", () => {
-    return request(app)
-      .get("/api/reviews")
-      .expect(200)
-      .then((response) => {
-        response.body.reviews.forEach((review) => {
-          expect(typeof review.review_id).toBe("number");
-          expect(typeof review.title).toBe("string");
-          expect(typeof review.created_at).toBe("string");
-          expect(typeof review.votes).toBe("number");
-          expect(typeof review.comment_count).toBe("string");
-          expect(typeof review.owner).toBe("string");
-          expect(typeof review.category).toBe("string");
-          expect(typeof review.review_img_url).toBe("string");
-          expect(typeof review.designer).toBe("string");
-          const input = Object.keys(
-            endpoint["GET /api/reviews"].exampleResponse
-          );
-          const expectedOutput = Object.keys(response.body);
-          expect(input).toMatchObject(expectedOutput);
-        });
+});
+test("/api/reviews", () => {
+  return request(app)
+    .get("/api/reviews")
+    .expect(200)
+    .then((response) => {
+      expect(response.body.reviews.length).toBe(13);
+      expect(response.body.reviews).toBeSortedBy("created_at", {
+        descending: true,
       });
-  });
+      response.body.reviews.forEach((review) => {
+        expect(typeof review.review_id).toBe("number");
+        expect(typeof review.title).toBe("string");
+        expect(typeof review.created_at).toBe("string");
+        expect(typeof review.votes).toBe("number");
+        expect(typeof review.comment_count).toBe("string");
+        expect(typeof review.owner).toBe("string");
+        expect(typeof review.category).toBe("string");
+        expect(typeof review.review_img_url).toBe("string");
+        expect(typeof review.designer).toBe("string");
+      });
+    });
 });
--- api/app.models.js
+++ api/app.models.js
@@ -13,16 +13,7 @@ exports.getReviewById = (id) => {
       return review.rows;
     });
 };
-exports.getReview = (sort_by = "created_at") => {
-  const validSortQueries = [
-    "created_at",
-    "votes",
-    "comment_count",
-    "review_id",
-  ];
-  if (!validSortQueries.includes(sort_by)) {
-    return Promise.reject({ status: 400, msg: "Invalid sort query!" });
-  }
+exports.getReview = () => {
   return db
     .query(
       "SELECT r.review_id, r.title, r.category, r.designer, r.owner, r.review_img_url, r.created_at, r.votes, COUNT(c.comment_id) AS comment_count FROM reviews AS r LEFT JOIN comments AS c ON r.review_id = c.review_id GROUP BY r.review_id ORDER BY created_at DESC"
